38f2a3a2279e591e8ffbcb736547b1980ba4c7df
Patrick Roberts <patrob22@gmail.com>
2023-03-19 22:45:06 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 2500) {
+    if (feet <= 2000) {
+      if (feet <= 400) {
+        return 'This one is on me!'
+      };
+      return 'That will be twenty bucks.'
+    };
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  };
+};
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? "Ok, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.'
+      break;
+  }
 }
